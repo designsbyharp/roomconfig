4b166517ce8ed557bde065c47eae1dd5c096a44c
3dDeveloper21 <3ddeveloper21@gmail.com>
2022-02-13 08:24:46 -0500

    landing page error with cta animation

--- modules/gsap/hero.js
+++ modules/gsap/hero.js
@@ -14,10 +14,12 @@ const ctaClickAnimations = () =>
         cta.addEventListener('click', (e)=>{
             heroTL
                 .set('.underline', {transformOrigin: "100% 0%"})
-                .to('.underline', {duration: 2, scaleX: 0, ease: "power3.inOut"})
-                .to('.hero__cover', {duration: 0.7, scaleY: 1, ease: "power3.inOut"}, "-=0.25")
+                .to('.underline', {duration: 0.7, scaleX: 0, ease: "power3.inOut"})
+                .to('.hero__cover', {duration: 0.7, scaleY: 1, ease: "power3.inOut"}, "-=0.1")
                 .set('.hero__cover', {transformOrigin: "50% 0%"})
                 .to('.hero__cover', {duration: 0.7, scaleY: 0, ease: "power3.inOut"})
+                .to('.hero', {opacity: 0}, '-=0.7')
+                .set('.hero', {display: 'none'})
         })
     })
 
